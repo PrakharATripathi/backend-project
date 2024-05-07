48fb521fd33659b2ea8f2de6be42faebf83e0461
PRAKHAR-A-TRIPATHI <151377167+PrakharATripathi@users.noreply.github.com>
2024-05-07 11:49:52 +0530

    utile code disscussion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 
 dotenv.config({path:'./env'})
 
-connectDB();
-
-
+connectDB()
+.then(()=>{
+    app.on("erorr", (error) => {
+        console.log("somthing wrong in database");
+        throw error
+    })
+    app.listen(process.env.PORT||3000,()=>{
+        console.log(`server start port=${process.env.PORT}`)
+    });
+})
+.catch((err)=>{console.log(err)});
 
 
 
